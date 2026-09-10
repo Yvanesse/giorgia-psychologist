660b98f49832a30c8ff1d9b4b497cd81cdb393ff
Yvanesse <149711726+Yvanesse@users.noreply.github.com>
2026-09-10 13:01:52 +0200

    refine: move articles gateway near page end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
       <ApproachSection />
       <JourneySection />
       <FormatsSection />
-      <ArticlesSection />
       <FaqSection />
+      <ArticlesSection />
       <FinalCtaSection />
     </main>
   );
